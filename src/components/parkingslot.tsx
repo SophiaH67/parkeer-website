9eb55f96ecfe34c3af64304896ca8c68aa47eaf3
marnixah <marnix@hagehoeve.com>
2021-04-15 18:11:09 +0200

    feat: made parkingslot component react to props

--- src/components/parkingslot.tsx
+++ src/components/parkingslot.tsx
@@ -1,11 +1,45 @@
 import { Box, Button, Grid, LinearProgress, Paper, Typography } from '@material-ui/core';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { parkingslot } from '../interfaces';
+import dayjs from 'dayjs';
+import 'dayjs/locale/nl';
+/// <reference types="@dayjs/relative-time" />
+import relativeTime from 'dayjs/plugin/relativeTime';
+/// <reference types="@dayjs/duration" />
+import duration from 'dayjs/plugin/duration';
+dayjs.extend(relativeTime);
+dayjs.extend(duration);
 
-function Parkingslot() {
+dayjs.locale('nl')
+let interval: NodeJS.Timeout;
+
+function Parkingslot(props: parkingslot) {
+  const [progress, setProgress] = useState(100);
+  const [durationLeft, setDurationLeft] = useState("loading...");
+  const startTime = dayjs(props.startTime);
+  const endTime = dayjs(props.endTime);
+  const diff = startTime.diff(endTime);
+
+  const updateProgress = () => {
+    setProgress(
+      Math.min(Math.max(
+        ((new Date().getTime() - props.startTime) / (props.endTime - props.startTime)) * 100
+        , 0), 100)
+    );
+    setDurationLeft(
+      dayjs.duration(endTime.valueOf() - new Date().getTime()).humanize()
+    )
+    
+  }
+  useEffect(() => {
+    interval = setInterval(updateProgress, 1000);
+    updateProgress();
+  }, []);
+
   return (
     <Paper style={{ padding: "1rem" }}>
       <Typography variant="h5">
-        U bent nog geparkeert voor XX minuten
+        U bent nog geparkeert voor {durationLeft}
       </Typography>
       <Grid
         container
@@ -15,15 +49,17 @@ function Parkingslot() {
         style={{ width: "100%" }}
       >
         <Grid item xs={6}>
-          <Typography variant="subtitle1">Some time - some time</Typography>
+          <Typography variant="subtitle1">{startTime.format("dddd DD HH:mm")}-{endTime.format("HH:mm")}</Typography>
+          <Typography variant="subtitle2">{props.licensePlate}</Typography>
         </Grid>
         <Grid item xs={6}>
-          <Button variant="contained" color="secondary" style={{float: "right", margin: "1rem"}}>
+          <Button variant="contained" color="secondary" style={{ float: "right", margin: "1rem" }}>
             annuleer
           </Button>
         </Grid>
       </Grid>
-      <LinearProgress />
+      <LinearProgress variant="determinate" value={((new Date().getTime() - props.startTime) / (props.endTime - props.startTime)) * 100} />
+
     </Paper>
   );
 }
