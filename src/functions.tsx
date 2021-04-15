ae5e52975538581e8fdb3cd3f229e958a4754282
marnixah <marnix@hagehoeve.com>
2021-04-15 19:13:50 +0200

    feat: added parkingslot manipulation functions

--- src/functions.tsx
+++ src/functions.tsx
@@ -1,1 +1,14 @@
 import React from 'react';
+import { parkingslot } from './interfaces'
+
+export const addParkingSlot = (parkingSlot: parkingslot) => {
+    let parkingSlots = getParkingSlots();
+    parkingSlots.push(parkingSlot);
+    localStorage.setItem('parkingSlots', JSON.stringify(parkingSlots));
+}
+
+export const getParkingSlots = ():parkingslot[] => {
+    let parkingSlots = localStorage.getItem('parkingSlots');
+    if (parkingSlots === null) parkingSlots = "[]";
+    return JSON.parse(parkingSlots);
+}
